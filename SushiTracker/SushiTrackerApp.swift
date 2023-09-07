291ddcae9a816454a3a70d9afccb3fa17c98f8b8
GiuAte <giulioaterno@gmail.com>
2023-09-07 10:03:00 +0200

    Initial Commit

---
+++ SushiTracker/SushiTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiTrackerApp.swift
+//  SushiTracker
+//
+//  Created by Giulio Aterno on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SushiTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
